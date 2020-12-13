dc898734e0fd330c384bad0afaa29ad9d1d9aa9d
raycktron <57748343+raycktron@users.noreply.github.com>
2020-12-13 13:37:25 +0700

    Pembuatan Login Page

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private Label labelKeterangan;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private void actionLogin(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void actionCancel(ActionEvent actionEvent) {
+    }
+}
